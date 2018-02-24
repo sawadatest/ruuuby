844a2c28e2263c49c3e846b73a40fa591bc9196d
吉永稔弘 <yoshinaga@yoshinagaminoruhiroshinoMacBook-Pro.local>
2018-02-25 08:18:44 +0900

    tenki

--- app/app/controllers/home_controller.rb
+++ app/app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
+API_KEY = "5b9841f5cac1ce98cf96708a291fad2f"
+BASE_URL = "http://api.openweathermap.org/data/2.5/forecast"
+require "json"
+require "open-uri"
+
 class HomeController < ApplicationController
+
   def top
-
-
-
-    @hoge="GREATEST!"
-
+    response = open(BASE_URL + "?q=tokyo,jp&APPID="+API_KEY)
+    gon.weather= response.read
   end
+
+
 end
